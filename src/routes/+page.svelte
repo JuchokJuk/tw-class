68d9c836e78761523f1fc4929bbd337854ba4167
JuchokJuk <juk.juchok@yandex.ru>
2024-12-02 23:20:15 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let condition = $state(false);
+</script>
+
+<button
+  class="text-[red]"
+  class:bg-[blue]={condition}
+  onclick={() => {
+    condition = !condition;
+  }}
+>
+  Welcome to SvelteKit
+</button>
